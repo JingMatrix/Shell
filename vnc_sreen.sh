0cbd844897d865411015f3ffec88b941e9ab236b
JingMatrix <jingmatrix@gmail.com>
2022-09-02 15:22:43 +0200

    update config

--- vnc_sreen.sh
+++ vnc_sreen.sh
@@ -1,11 +1,5 @@
 #! /bin/zsh
 
-host_ip="10.245.5.70"
-if [[ "$(hostname -I)" != "$host_ip " ]]; then
-	notify-send "Not start vnc" "Current network is not ready for safe vnc"
-	exit
-fi
-
 if swaymsg -t get_outputs | grep '"name": "HEADLESS-1"'; then
 	echo "Output HEADLESS-1 exists"
 else
