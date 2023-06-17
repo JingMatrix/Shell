8cbf891407f9019cfcba8c9ab23614299214828f
JingMatrix <jingmatrix@gmail.com>
2023-06-18 00:29:59 +0200

    Add mimeapps

--- update_symbolic_links.sh
+++ update_symbolic_links.sh
@@ -35,6 +35,16 @@ elif ! cmp $basedir/config/ly /etc/pam.d/ly; then
 	sudo cp $basedir/config/ly /etc/pam.d
 fi
 
+ln -f $basedir/config/mimeapps.list
+if [[ ! -f $HOME/.config/mimeapps.list ]]; then
+	echo "Install mimeapps.list"
+	ln -f $basedir/config/mimeapps.list $HOME/.config/mimeapps.list
+	ln -sf $HOME/.config/mimeapps.list $HOME/.local/share/applications/mimeapps.list
+elif ! cmp $basedir/config/mimeapps.list $HOME/.config/mimeapps.list; then
+	echo "Update mimeapps locally"
+	cp $HOME/.config/mimeapps.list $basedir/config/mimeapps.list
+fi
+
 if [[ -d $HOME/Documents/Project/qutebrowser ]]; then
 	mkdir -p $HOME/.local/bin
 	ln -sf $basedir/archives/qutebrowser $HOME/.local/bin
