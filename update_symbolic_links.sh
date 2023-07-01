fcd28d7b024fd7f3123137bc4ed5fd7b0f190a32
JingMatrix <jingmatrix@gmail.com>
2023-07-01 16:09:12 +0200

    Add fcitx5 pinyin.conf

--- update_symbolic_links.sh
+++ update_symbolic_links.sh
@@ -31,6 +31,7 @@ ln -sf $basedir/config/clash.yaml $HOME/.config/clash
 mkdir -p $HOME/.config/fcitx5/conf
 ln -sf $basedir/config/fcitx5.conf $HOME/.config/fcitx5/config
 ln -sf $basedir/config/fcitx5_classicui.conf $HOME/.config/fcitx5/conf/classicui.conf
+ln -sf $basedir/config/fcitx5_pinyin.conf $HOME/.config/fcitx5/conf/pinyin.conf
 # Profile is always rewritten by fcitx5, it seems we must need a GUI to configure it.
 # This config is thus not reliable.
 cp $basedir/config/fcitx5.profile $HOME/.config/fcitx5/profile
